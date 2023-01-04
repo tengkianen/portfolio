9dc7aaa1b6480dd9bcf01c65eeabba9da8cd951b
tengkianen <tengkianen@gmail.com>
2023-01-04 22:41:18 +0800

    removed unwanted copyright logo

--- frontend/src/wrapper/AppWrap.js
+++ frontend/src/wrapper/AppWrap.js
@@ -7,10 +7,6 @@ const AppWrap = (Component, idName, classNames) => function HOC() {
         <SocialMedia />
         <div className='app__wrapper app__flex'>
             <Component />
-            <div className='copyright'>
-                <p className='p-text'>@2022</p>
-                <p className='p-text'>ALL RIGHTS RESERVED</p>
-            </div>
         </div>
         <NavigationDots active={idName} />
     </div>
